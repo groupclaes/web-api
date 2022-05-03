5d94ecf6fe57cc3295c7cfda5a4b96d0f8835bb2
Jamie Vangeysel <jamie.vangeysel@simplintho.com>
2022-05-03 11:13:49 +0200

    fix routes

--- backend/distribution_subscribers/src/index.js
+++ backend/distribution_subscribers/src/index.js
@@ -2,7 +2,7 @@
 
 const Fastify = require('@groupclaes/fastify-elastic')
 const config = require('./config')
-const { routes } = require('./routes')
+const routes = require('./routes')
 
 /** Main loop */
 const main = async () => {
