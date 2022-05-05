79a87f0f82644fe404f504aec96b2d0a350b4853
Jamie Vangeysel <jamie.vangeysel@simplintho.com>
2022-05-05 11:21:50 +0200

    fix dir, create update and build script

---
+++ build.sh
@@ -0,0 +1,28 @@
+cd ~/development/groupclaes/web-api
+
+cd ./backend/distribution_subscribers
+sh ./build.sh
+
+cd ../groupclaes_blogposts
+sh ./build.sh
+
+cd ../groupclaes_client-map
+sh ./build.sh
+
+cd ../groupclaes_solicitations
+sh ./build.sh
+
+cd ../groupclaes_vacancies
+sh ./build.sh
+
+cd ../openedge
+sh ./build.sh
+
+cd ../profile
+sh ./build.sh
+
+cd ../stats
+sh ./build.sh
+
+cd ../vat-check
+sh ./build.sh
---
+++ update.sh
@@ -0,0 +1,28 @@
+cd ~/development/groupclaes/web-api
+
+cd ./backend/distribution_subscribers
+npm update
+
+cd ../groupclaes_blogposts
+npm update
+
+cd ../groupclaes_client-map
+npm update
+
+cd ../groupclaes_solicitations
+npm update
+
+cd ../groupclaes_vacancies
+npm update
+
+cd ../openedge
+npm update
+
+cd ../profile
+npm update
+
+cd ../stats
+npm update
+
+cd ../vat-check
+npm update
