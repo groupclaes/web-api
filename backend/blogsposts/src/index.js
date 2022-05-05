7c520f16bf1c1d46d95e1e50d50805db9a9e1c27
Jamie Vangeysel <jamie.vangeysel@simplintho.com>
2022-05-05 10:29:58 +0200

    preHandler is included in fastify-elastic v1.0.17

--- backend/blogsposts/src/index.js
+++ backend/blogsposts/src/index.js
@@ -7,16 +7,6 @@ const routes = require('./routes')
 const main = async () => {
   const fastify = new Fastify(config.wrapper)
   fastify.routeMultiple(routes)
-  fastify.server.addHook('preHandler', (request, reply, next) => {
-    reply.header('Content-Security-Policy', `default-src 'none'`)
-    reply.header('X-Content-Type-Options', `nosniff`)
-    reply.header('X-Frame-Options', `DENY`)
-    reply.header('X-Xss-Protection', `1; mode=block`)
-    reply.header('Referrer-Policy', `no-referrer`)
-    reply.header('Permissions-Policy', `fullscreen=*`)
-    reply.header('Strict-Transport-Security', `max-age=15552000; preload`)
-    next()
-  })
   await fastify.start()
 }
 
